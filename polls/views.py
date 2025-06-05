003526bba8b773cd66fcb3998713292e5ba1c1e0
Luis <luismcampos32@hotmail.com>
2025-06-04 21:54:52 -0500

    Replaced old index, detail, and results views using Django generic views

--- polls/views.py
+++ polls/views.py
@@ -2,30 +2,28 @@ from django.db.models import F
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
-
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    # Dictionary to pass data to the template
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
-    # Get the question object or return a 404 error if not found
-    question = get_object_or_404(Question, pk=question_id)
-    # Render the detail template with the question object
-    # The template will have access to the 'question' variable
-    # and can display its attributes like question_text and pub_date
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls.results.html"
 
 
 def vote(request, question_id):

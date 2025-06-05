82a3977b32ec854275b5e782b833ce72b6ea2f84
Luis <luismcampos32@hotmail.com>
2025-06-05 18:23:16 -0500

    Customized how admin page adds related objects

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Question
+
+from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
+
+# The following class defines how the Choice model will be displayed as an inline within the Question model in the admin interface.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+# The following class defines how the Question model will be displayed in the admin interface.
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+# The following line registers the Question model with the admin site using the QuestionAdmin class.
+admin.site.register(Question, QuestionAdmin)
